f60b7180e637595b01cb021961bb06e328f83d32
Madalina Barburescu <37048495+mariabarburescu@users.noreply.github.com>
2019-02-11 13:21:37 +0200

    Add files via upload

---
+++ homework.c
@@ -0,0 +1,16 @@
+#include "homework.h"
+
+int num_threads;
+int resize_factor;
+
+void readInput(const char * fileName, image *img) {
+
+}
+
+void writeData(const char * fileName, image *img) {
+
+}
+
+void resize(image *in, image * out) { 
+
+}
---
+++ homework.h
@@ -0,0 +1,14 @@
+#ifndef HOMEWORK_H
+#define HOMEWORK_H
+
+typedef struct {
+
+}image;
+
+void readInput(const char * fileName, image *img);
+
+void writeData(const char * fileName, image *img);
+
+void resize(image *in, image * out);
+
+#endif /* HOMEWORK_H */
